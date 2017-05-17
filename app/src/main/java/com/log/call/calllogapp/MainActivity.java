7743f07f3fca2f5fa157a50fc3753bbd1b71a026
charlig <nanayakkaraoffice@gmail.com>
2017-05-18 01:56:14 +0530

    init

--- app/src/main/java/com/log/call/calllogapp/MainActivity.java
+++ app/src/main/java/com/log/call/calllogapp/MainActivity.java
@@ -1,15 +1,14 @@
 package com.log.call.calllogapp;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.os.Bundle;
 import android.provider.CallLog;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
